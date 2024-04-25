df4b094e11fa3ca1add774001c3b185afd615ab2
Alexander Sapountzis <alexander.sapountzis@gmail.com>
2024-04-25 12:56:17 -0400

    Add Point Free Solution

--- point-free/ex.js
+++ point-free/ex.js
@@ -1,29 +1,39 @@
-"use strict";
+'use strict';
 
-function output(txt) {
-	console.log(txt);
-}
+// function output(txt) {
+//     console.log(txt);
+// }
+
+var output = console.log.bind(console);
 
 function printIf(shouldPrintIt) {
-	return function(msg) {
-		if (shouldPrintIt(msg)) {
-			output(msg);
-		}
-	};
+    return function (msg) {
+        if (shouldPrintIt(msg)) {
+            output(msg);
+        }
+    };
 }
 
 function isShortEnough(str) {
-	return str.length <= 5;
+    return str.length <= 5;
 }
 
-function isLongEnough(str) {
-	return !isShortEnough(str);
+function not(fn) {
+    return function negated(...args) {
+        return !fn(...args);
+    };
 }
 
-var msg1 = "Hello";
-var msg2 = msg1 + " World";
+// function isLongEnough(str) {
+//     return !isShortEnough(str);
+// }
 
-printIf(isShortEnough)(msg1);		// Hello
+var isLongEnough = not(isShortEnough);
+
+var msg1 = 'Hello';
+var msg2 = msg1 + ' World';
+
+printIf(isShortEnough)(msg1); // Hello
 printIf(isShortEnough)(msg2);
 printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2);		// Hello World
+printIf(isLongEnough)(msg2); // Hello World
